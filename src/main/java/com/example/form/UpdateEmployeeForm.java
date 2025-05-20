204f1b2a37084ebeff160a12ec9feea6b017e761
0114Koki <koki.kurihara.0114@gmail.com>
2025-05-20 17:06:09 +0900

    UpdateEmployeeForm complete

--- src/main/java/com/example/form/UpdateEmployeeForm.java
+++ src/main/java/com/example/form/UpdateEmployeeForm.java
@@ -1,4 +1,38 @@
 package com.example.form;
 
+
+/**
+ * 従業員情報をUpdateするときに使用するForm
+ *
+ * @author koki.kurihara
+ */
 public class UpdateEmployeeForm {
+    /** 従業員ID */
+    private String id;
+    /** 扶養人数 */
+    private String dependentsCount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDependentsCount() {
+        return dependentsCount;
+    }
+
+    public void setDependentsCount(String dependentsCount) {
+        this.dependentsCount = dependentsCount;
+    }
+
+    @Override
+    public String toString() {
+        return "UpdateEmployeeForm{" +
+                "id='" + id + '\'' +
+                ", dependentsCount='" + dependentsCount + '\'' +
+                '}';
+    }
 }
