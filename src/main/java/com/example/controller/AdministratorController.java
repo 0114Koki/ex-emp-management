e2b282bd8fd6016e6f006e3892f663d87af5ff16
0114Koki <koki.kurihara.0114@gmail.com>
2025-05-20 14:14:27 +0900

    AdministratorController complete

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,10 +1,27 @@
 package com.example.controller;
 
 
+import com.example.form.InsertAdministratorForm;
+import com.example.service.AdministratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * 管理者登録画面を表示する処理を記述する
  *
  * @author koki.kurihara
  */
+@Controller
+@RequestMapping("/")
 public class AdministratorController {
+    @Autowired
+    private AdministratorService administratorService;
+
+    @GetMapping("to-insert")
+    public String toInsert(InsertAdministratorForm form){
+        return "administrator/insert";
+    }
 }
