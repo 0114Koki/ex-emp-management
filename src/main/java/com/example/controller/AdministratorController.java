8b25b59a8bb87f8822238672c6f560b2edaf930d
0114Koki <koki.kurihara.0114@gmail.com>
2025-05-20 14:41:23 +0900

    login画面 complete

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 
 import com.example.form.InsertAdministratorForm;
+import com.example.form.LoginForm;
 import com.example.service.AdministratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +20,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdministratorController {
     @Autowired
     private AdministratorService administratorService;
+
+    /**
+     * ログイン画面に遷移する
+     *
+     * @param form
+     * @return
+     */
+    @GetMapping("/")
+    public String toLogin(LoginForm form){
+        return "administration/login";
+    }
+
+
 
     /**
      * 管理者情報を登録する
